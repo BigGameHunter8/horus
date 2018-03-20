9f6f2c14523532725765a975de4496d79ddc4a78
Dima <admin@Admins-iMac-2.local>
2018-03-20 17:54:36 +0200

    validation

--- horus_poject/js/main.js
+++ horus_poject/js/main.js
@@ -1,16 +1,45 @@
-function validate(){
+function validate(e){
+
     var phone = document.getElementById("phone");
     var error = document.getElementById("error");
-    
-    console.log(error);
+
+    error.innerHTML = "";
     
     if(isNaN(phone.value)){
         error.innerHTML="Введите только цифры";
-        console.log(error);
+        e.preventDefault();
     }
 
-    else if(phone.value=""){
-        error.innerHTML="Вы не вели телефон";
-        console.log(error);
+    else if(phone.value==""){
+        error.innerHTML="Вы не ввели телефон";
+        e.preventDefault();
+    }
+
+    var s_phone=document.getElementById("s_phone");
+    var s_error=document.getElementById("s_error");
+
+    s_error.innerHTML="";
+
+    if(isNaN(s_phone.value)){
+        s_error.innerHTML="Введите только цифры";
+        e.preventDefault();
+    }
+
+    else if(s_phone.value==""){
+        s_error.innerHTML="Вы не ввели телефон";
+        e.preventDefault();
     }
 }
+
+
+var form =document.getElementById("form");
+form.addEventListener("submit",validate);
+
+$(document).ready(function(){
+
+    //active_form_input
+    
+
+   
+    
+});
